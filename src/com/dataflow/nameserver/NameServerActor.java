30a101e47762e85b38da58dd9271cc743325af65
kanthkumar <kkd6428@rit.edu>
2016-05-05 02:34:14 -0400

    ClusterManager and WokerNode Failure Handler

--- src/com/dataflow/nameserver/NameServerActor.java
+++ src/com/dataflow/nameserver/NameServerActor.java
@@ -1,30 +1,32 @@
 package com.dataflow.nameserver;
 
 
-import com.dataflow.messages.RegisterWorker;
-import com.dataflow.messages.WorkRequest;
 import com.dataflow.workers.HeartBeatActor.HBMessage;
 import com.typesafe.config.Config;
 
+import akka.actor.ActorRef;
 import akka.actor.ActorSelection;
+import akka.actor.Props;
 import akka.actor.UntypedActor;
 
 public class NameServerActor extends UntypedActor{
 
 	private ActorSelection jobManager;
+	private ActorRef clusterManager;
 
 	public NameServerActor(Config config) {
 		this.jobManager = getContext().system().actorSelection
 				(config.getString("akka.actor.job-manager"));
+		this.clusterManager = getContext().actorOf(Props.
+				create(ClusterManager.class), "ClusterManager");
 	}
 	
 	@Override
 	public void onReceive(Object msg) throws Exception {
-		if(msg instanceof HBMessage){
-			HBMessage hbMsg = (HBMessage) msg;
-			System.out.println(hbMsg);
+		if(msg instanceof HBMessage) {
+			clusterManager.tell(getSender(), getSelf());
 		}
-		else if(msg instanceof RegisterWorker || msg instanceof WorkRequest) {
+		else {
 			jobManager.tell(msg, getSelf());
 		}
 	}
